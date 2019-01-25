0b737a3c455e8fe7713b26e756a762beee68b8cd
Nathanovich <n.flier@live.nl>
2019-01-25 22:48:17 +0100

    Implemented basic movement and rotating world into a new scene (Game.unity)

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+	public Transform world;
+	public int playerNumber = 1;
+	public float moveSpeed = 3f;
+
+	private void Update() {
+		float input = Input.GetAxis("Horizontal_" + playerNumber.ToString());
+		float movement = input * Time.deltaTime * moveSpeed;
+		transform.RotateAround(world.position, Vector3.forward, movement);
+	}
+
+}
---
+++ Assets/Scripts/World.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class World : MonoBehaviour {
+
+	public float rotateSpeed;
+
+    void Start() {
+        
+    }
+
+    void Update() {
+		transform.Rotate(Vector3.forward, rotateSpeed * Time.deltaTime);
+    }
+
+}
