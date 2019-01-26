94434a69c37dd6b09da01d9cbffb57a8c4a4e41e
Nathanovich <n.flier@live.nl>
2019-01-26 16:25:37 +0100

    Added charge rocket plates to world (unfunctional yet)

---
+++ Assets/Scripts/RocketMeter.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class RocketMeter : MonoBehaviour {
+
+	[SerializeField] private Image fillImage;
+	private bool isActive = true;
+	private float increase = 1f;
+	private float currentFill = 0f;
+	private float increaseSpeed = 1f;
+	private float increaseDirection = 1f;
+
+    private void Start() {
+        
+    }
+	
+    private void Update() {
+		if (isActive) {
+			currentFill += increaseSpeed * increaseDirection * Time.deltaTime;
+			if (currentFill >= 1f) {
+
+			}
+		}
+    }
+
+	public void ActivateMeter() {
+
+	}
+
+}
