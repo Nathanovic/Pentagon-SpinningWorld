862728260b94239bf69bfaf583086e40fbd919db
lb321 <lucas.001@live.nl>
2019-01-26 13:32:22 +0100

    Falling meteors have 3d model and are rotating

--- Assets/Scripts/Meteor.cs
+++ Assets/Scripts/Meteor.cs
@@ -8,14 +8,20 @@ public class Meteor : MonoBehaviour {
     private bool isGrounded = false;
     public bool containsResource = false;
     public ParticleSystem vfxImpact;
+    public GameObject visuals;
     
     public delegate void ImpactFunction();
     public event ImpactFunction onImpact;
+
+    private Vector3 rotation;
 
     private void Start() {
         worldBody = GetComponent<WorldBody>();
         isGrounded = World.Instance.GetGrounded(worldBody);
         onImpact += ShowImpactParticles;
+        
+        Quaternion randomRotation = Random.rotation;
+        rotation = new Vector3(randomRotation.x, randomRotation.y, randomRotation.z);
     }
 
     private void Update() {
@@ -30,6 +36,9 @@ public class Meteor : MonoBehaviour {
                 //TODO: show destroy meteor particles
             }
         }
+        
+        if(!isGrounded)
+            visuals.transform.Rotate(rotation);
     }
 
 
