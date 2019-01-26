24a63e7b61c926aa4a04fd189df1ced3b9ed6c17
lb321 <lucas.001@live.nl>
2019-01-26 11:26:26 +0100

    Chooses between two meteors to spawn

--- Assets/Scripts/MeteorSpawner.cs
+++ Assets/Scripts/MeteorSpawner.cs
@@ -1,7 +1,12 @@
 ﻿using UnityEngine;
 
 public class MeteorSpawner : MonoBehaviour {
-    public GameObject meteorPrefab;
+    public GameObject meteorPrefab1;
+    public float spawnChangeMeteor1 = 0.9f;
+    public float spawnChangeMeteor2 = 0.1f;
+    private float totalSpawnChange = 1;
+    public GameObject meteorPrefab2;
+    
     public float spawnInterval = 1;
     public float offset = 15.0f;
     public float timeTillSpawn = 0.0f;
@@ -11,19 +16,27 @@ public class MeteorSpawner : MonoBehaviour {
     public float minMeteorScale = 0.5f;
     public float maxMeteorScale = 2;
 
+    private void Start() {
+        totalSpawnChange = spawnChangeMeteor1 + spawnChangeMeteor2;
+    }
+
     // Update is called once per frame
     void Update() {
         timeTillSpawn -= Time.deltaTime;
 
         if (timeTillSpawn <= 0) {
-            SpawnMeteor();
+            float random = Random.Range(0, totalSpawnChange);
+            if (random <= spawnChangeMeteor1)
+                SpawnMeteor(meteorPrefab1);
+            else 
+                SpawnMeteor(meteorPrefab2);
             timeTillSpawn = spawnInterval;
         }
     }
 
-    private void SpawnMeteor() {
+    private void SpawnMeteor(GameObject meteorToSpawn) {
         int spawnAngle = Random.Range(0, 360);
-        GameObject meteor = Instantiate(meteorPrefab, new Vector3(Mathf.Sin(spawnAngle) * offset, Mathf.Cos(spawnAngle) * offset, 1), Quaternion.identity);
+        GameObject meteor = Instantiate(meteorToSpawn, new Vector3(Mathf.Sin(spawnAngle) * offset, Mathf.Cos(spawnAngle) * offset, 1), Quaternion.identity);
         WorldBody worldBody = meteor.GetComponent<WorldBody>();
         float speed1 = Random.Range(minMeteorFallingSpeed, maxMeteorFallingSpeed);
         float speed2 = Random.Range(minMeteorFallingSpeed, maxMeteorFallingSpeed);
